942c172346734b6900aac1e0270a9806d51afd56
Kenneth <kenneth.ng.5226@outlook.com>
2021-05-07 22:36:44 +0100

    Rename built-in validators

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,10 +1,10 @@
 /// maps types to the names of their corresponding validators. for example,
 /// a field of type [String] should be validated by a StringValidator.
 const validatorMap = <String, String>{
-  'String': 'StringValidator',
-  'int': 'NumberValidator',
-  'double': 'NumberValidator',
-  'num': 'NumberValidator',
-  'bool': 'BoolValidator',
+  'String': 'StringMust',
+  'int': 'NumberMust',
+  'double': 'NumberMust',
+  'num': 'NumberMust',
+  'bool': 'BoolMust',
   'dynamic': 'Validator',
 };
